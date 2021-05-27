8402b38cedbe1053f72c61333aeb35bbeca2ee1d
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-05-26 22:38:35 -0400

    adding my magazine section for my homepage

--- client/src/pages/PublicRoute/index.js
+++ client/src/pages/PublicRoute/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./style.css"
 
 
 /* This is a very simple component.. it probably doesn't need to be a smart component at this point but you never know what's goingto happen in the future */
@@ -6,19 +7,158 @@ import React from 'react'
 function PublicRoute() {
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-sm">
-                </div>
-                <div className="col-sm">
-                    <h3>Home Page</h3>
-                    <h6>PUBLIC ROUTE: Anyone can see this content</h6>
-                </div>
-                <div className="col-sm">
+    <>   
+        <div className="scrollmenu">
 
-                </div>
-            </div>
+        <div className="desc">
+        <a href="https://lifeandhealth.org/nutrition/morning-coffee-can-impair-blood-sugar-response/1520002.html"><img src="https://lifeandhealth.org/wp-content/uploads/2021/04/shutterstock_236589346-600x600.jpg"></img></a>
+        <div className="infosec">
+        <h6>Morning Coffee Can Impair <br></br>Blood Sugar Response</h6>
+            <p>by: DANNY KWON JD</p>
+          </div> 
         </div>
+
+
+
+        <div className="desc">
+        <a href="https://lifeandhealth.org/fitness/do-you-really-need-10000-steps-per-day/1719782.html"><img src="https://lifeandhealth.org/wp-content/uploads/2021/02/shutterstock_274885196-600x600.jpg"></img></a>
+        <div class="infosec">
+            <h6>Do You Really Need 10,000<br></br> Steps Per Day?</h6>
+                <p>by: DANNY KWON JD</p>
+              </div> 
+    </div>
+
+
+    <div className="desc">
+        <a href="https://lifeandhealth.org/lifestyle/intermittent-fasting-does-it-work/1017874.html"><img src="https://lifeandhealth.org/wp-content/uploads/2020/02/shutterstock_209969872-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>Intermittent Fasting <br></br> Does It Work?</h6>
+                <p>by: HARVEY HAHN, MD, FACC</p>
+              </div> 
+    </div>
+
+
+    <div className="desc">
+        <a href="https://lifeandhealth.org/fitness/3-questions-to-ask-yourself-when-setting-a-fitness-goal/1014895.html"><img src="https://lifeandhealth.org/wp-content/uploads/2019/03/jeremy-lapak-553145-unsplash-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>3 Questions To Ask Yourself<br></br>When Setting A Fitness Goal</h6>
+                <p>by: PATRICK WOOD, ATC, CSCS</p>
+              </div> 
+    </div>
+
+
+
+    <div className="desc">
+        <a href="https://lifeandhealth.org/lifestyle/how-to-get-better-sleep/1717188.html"><img src="https://lifeandhealth.org/wp-content/uploads/2019/09/vladislav-muslakov-CwIU33KGToc-unsplash-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>How To Get Better Sleep</h6>
+                <p>by: HARVEY HAHN, MD, FACC</p>
+              </div> 
+    </div>
+
+
+    <div className="desc">
+        <a href="https://lifeandhealth.org/fitness/the-7-keys-to-a-long-and-healthy-life-exercise/0214317.html"><img src="https://lifeandhealth.org/wp-content/uploads/2019/01/marek-szturc-659613-unsplash-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>The 7 Keys To A Long And<br></br>Healthy Life: Exercise</h6>
+                <p>by: WESTNEY WHITE</p>
+              </div> 
+          </div>
+
+
+          <div className="desc">
+            <a href="https://lifeandhealth.org/fitness/5-ways-to-keep-your-motivation-to-exercise/1514065.html"><img src="https://lifeandhealth.org/wp-content/uploads/2019/01/rawpixel-1162836-unsplash-600x600.jpg"></img></a> 
+            <div className="infosec">
+                <h6>5 Ways To Keep Your<br></br> Motivation To Exercise</h6>
+                    <p>by: WESTNEY WHITE</p>
+            </div> 
+    </div> 
+    </div>
+    
+
+
+
+    <div className="scrollmenu">
+
+
+
+    <div className="desc">
+        <a href="https://lifeandhealth.org/lifestyle/the-truth-about-burning-and-storing-fat/08406.html"><img src="https://lifeandhealth.org/wp-content/uploads/2018/12/vera-cho-789594-unsplash-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>The Truth About Burning<br></br> And Storing Fat</h6>
+                <p>by: DICK NUNEZ FT</p>
+              </div> 
+    </div>
+
+    <div className="desc">
+        <a href="https://lifeandhealth.org/nutrition/whats-the-big-deal-about-protein/1611935.html"><img src="https://lifeandhealth.org/wp-content/uploads/2018/10/rawpixel-1071089-unsplash-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>What’s The Big Deal About Protein?</h6>
+                <p>by: RAEANN LEAL</p>
+              </div> 
+            </div>
+
+
+            <div className="desc">
+        <a href="https://lifeandhealth.org/lifestyle/can-your-diet-and-lifestyle-affect-your-vision/1611990.html"><img src="https://lifeandhealth.org/wp-content/uploads/2018/10/amanda-dalbjorn-258807-unsplash-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>Can Your Diet And Lifestyle<br></br> Affect Your Vision?</h6>
+                <p>by: JUSTIN LEAL</p>
+              </div> 
+            </div>
+
+
+            <div className="desc">
+        <a href="https://lifeandhealth.org/nutrition/the-surprising-connection-between-diet-and-alzheimers-disease/0911063.html"><img src="https://lifeandhealth.org/wp-content/uploads/2018/07/jess-watters-701054-unsplash-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>What’s The Connection Between <br></br>Diet And Alzheimer’s Disease?</h6>
+                <p>by: RAEANN LEAL</p>
+              </div>
+            </div>
+
+
+            <div className="desc"> 
+        <a href="https://lifeandhealth.org/lifestyle/your-gut-health-and-why-you-should-care-about-it/109138.html"><img src="https://lifeandhealth.org/wp-content/uploads/2018/05/UMH_C_OBESE-MICRo@1x-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>Your Gut Health And Why <br></br>You Should Care About It</h6>
+                <p>by: RAEANN LEAL</p>
+              </div> 
+            </div>
+
+
+            <div className="desc">
+        <a href="https://lifeandhealth.org/healing/natural-remedies-a-brief-history/1711827.html"><img src="https://lifeandhealth.org/wp-content/uploads/2018/10/marie-grob-1040982-unsplash-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>Natural Remedies: A Brief History</h6>
+                <p>by: GRACE JAUWENA</p>
+              </div> 
+            </div>
+
+
+<div className="desc">
+        <a href="https://lifeandhealth.org/nutrition/turmeric-the-what-the-why-and-the-how/1511117.html"><img src="https://lifeandhealth.org/wp-content/uploads/2018/07/taylor-kiser-361164-unsplash-600x600.jpg"></img></a>
+        <div className="infosec">
+            <h6>Turmeric: The What, The Why,<br></br> And The How</h6>
+                <p>by:JUSTIN LEAL </p>
+              </div> 
+            </div>
+
+            <div className="desc">
+            <a href="https://lifeandhealth.org/food/why-plant-based/an-interview-with-a-plant-based-doctor-and-autoimmune-disease-cookbook-author/0815050.html"><img src="https://lifeandhealth.org/wp-content/uploads/2019/04/asdf-600x460.png"></img></a>
+        <div className="infosec">
+            <h6>We Sit Down With Plant-Based <br></br>Doctor And Autoimmune Disease <br></br>Cookbook Author, Dr. Joyce Choe</h6>
+                <p>by: GRACE JAUWENA</p>
+              </div> 
+            </div>  
+   </div> 
+</>
+
+    
+                
+
+
+    
+
     )
 }
 
