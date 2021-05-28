dfc5c7f37593ae6802b72b511ff63e2d53ef5e4e
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-05-27 21:42:08 -0400

    added recipes section functionality

--- client/src/pages/ProtectedRouteTwo/index.js
+++ client/src/pages/ProtectedRouteTwo/index.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useContext } from 'react'
-import Card from "../../components/Card"
+import React, { useEffect, useContext, useState } from 'react'
+import MealList from "../../components/MealList/MealList";
 import { UserContext } from "../../utils/UserContext";
 
 function ProtectedRoute() {
+	const [mealData, setMealData] = useState(null);
+	const [calories, setCalories] = useState(2000);
 
 	const [user, dispatch] = useContext(UserContext)
 	console.log(user)
@@ -28,15 +30,35 @@ function ProtectedRoute() {
 				console.log('Error fetching authorized user.');
 			});
 
-	}, []);
-
+	}, );
+	function getMealData() {
+		fetch(
+		  `https://api.spoonacular.com/mealplanner/generate?apiKey=cb1c464d94f142c08b156c5beddade8b&timeFrame=day&targetCalories=${calories}`
+		)
+		  .then((response) => response.json())
+		  .then((data) => {
+			setMealData(data);
+		  })
+		  .catch(() => {
+			console.log("error");
+		  });
+	  }
+	
+	  function handleChange(e) {
+		setCalories(e.target.value);
+	  }
 	return (
-		<div className="container">
-		
-			<Card>
-				<p>Hellloooooooooo</p>
-			</Card>
-		</div>
+		<div className="App">
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Calories (e.g. 2000)"
+          onChange={handleChange}
+        />
+        <button onClick={getMealData}>Get Daily Meal Plan</button>
+      </section>
+      {mealData && <MealList mealData={mealData} />}
+    </div>
 	)
 
 }
