7d5d05384d5effc59e33f3bd325c2ee8b44208a1
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-05-29 22:08:23 -0400

    added my voices section. Added 3 compponents folders, Editpost, createnewpost and allpost, and 2nd public page. downloaded needed npm packages

--- client/src/pages/PublicRouteTwo/index.js
+++ client/src/pages/PublicRouteTwo/index.js
@@ -1,15 +1,145 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+import CreateNewPost from "../../components/CreateNewPost/CreateNewPost";
+import Post from "../../components/AllPost/Post";
+import ModifyPost from "../../components/EditPost/ModifyPost"
+import bgImg from "./bgimg.jpg"
 
 
 
-function PublicRouteTwo() {
+function PublicRouteTwo () {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [allPosts, setAllPosts] = useState([]);
+  const [isCreateNewPost, setIsCreateNewPost] = useState(false);
+  const [isModifyPost, setIsModifyPost] = useState(false);
+  const [editPostId, setEditPostId] = useState("");
 
+  // Initialize useRef
+  const getTitle = useRef();
+  const getContent = useRef();
+
+  const savePostTitleToState = event => {
+    setTitle(event.target.value);
+  };
+  const savePostContentToState = event => {
+    setContent(event.target.value);
+  };
+  const toggleCreateNewPost = () => {
+    setIsCreateNewPost(!isCreateNewPost);
+  };
+  const toggleModifyPostComponent = () => {
+    setIsModifyPost(!isModifyPost)
+  }
+  const editPost = id => {
+    setEditPostId(id);
+    console.log(id)
+    toggleModifyPostComponent();
+  };
+  const deletePost = id => {
+    const modifiedPost = allPosts.filter(eachPost => {
+      return eachPost.id !== id;
+    });
+    setAllPosts(modifiedPost);
+  };
+  const updatePost = (event) => {
+    event.preventDefault();
+    const updatedPost = allPosts.map(eachPost => {
+      if (eachPost.id === editPostId) {
+        console.log([eachPost.id, editPostId] )
+        return {
+          ...eachPost,
+          title: title || eachPost.title,
+          content: content || eachPost.content
+        };
+      }
+      console.log(eachPost)
+      return eachPost;
+    });
+    setAllPosts(updatedPost);
+    toggleModifyPostComponent();
+  };
+  const savePost = event => {
+    event.preventDefault();
+    const id = Date.now();
+    setAllPosts([...allPosts, { title, content, id }]);
+    console.log(allPosts);
+    setTitle("");
+    setContent("");
+    getTitle.current.value = "";
+    getContent.current.value = "";
+    toggleCreateNewPost();
+  };
+  if (isCreateNewPost) {
     return (
-<>
-<h1>Heyyyy its the voices secetion</h1>
-</>
-    )
-    }
+      <>
+        <CreateNewPost
+          savePostTitleToState={savePostTitleToState}
+          savePostContentToState={savePostContentToState}
+          getTitle={getTitle}
+          getContent={getContent}
+          savePost={savePost}
+          deletePost={deletePost}
+        />
+      </>
+    );
+  }
+  else if (isModifyPost) {
+    const post = allPosts.find(post => {
+      return post.id === editPostId;
+    });
+    return (
+      <ModifyPost
+        title={post.title}
+        content={post.content}
+        updatePost={updatePost}
+        savePostTitleToState={savePostTitleToState}
+        savePostContentToState={savePostContentToState}
+      />
+    );
+  }
+  return (
+    <>
+      <div className="bgImg">
+      <h1>Voices <br/> from you!</h1>
+        <img
+          
+          src={bgImg}
+        />
+      </div>
+      {!allPosts.length ? (
+        <section className="no-post">
+          <h1>No Post Found!</h1>
+          <br />
+          <br />
+          <section className="button-wrapper">
+      <button onClick={toggleCreateNewPost} className="button">Create New</button>
+      </section>
+        </section>
+      ) : (
+      <div><h1>All Posts</h1>
+        <section className="all-post">
+        {allPosts.map(eachPost => {
+          return (
+            <Post
+              id={eachPost.id}
+              key={eachPost.id}
+              title={eachPost.title}
+              content={eachPost.content}
+              editPost={editPost}
+              deletePost={deletePost}
+            />
+          );
+        })}
+      <section className="button-wrapper">
+      <button onClick={toggleCreateNewPost} className="button">Create New</button>
+      </section>
+        </section>
+        
+        </div>
+      )}
 
-
-export default PublicRouteTwo
+      
+    </>
+  );
+};
+export default PublicRouteTwo;
