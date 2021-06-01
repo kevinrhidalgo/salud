1b6963a09fbd2441fbd1ec2e39fa61d5292801e8
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-05-31 22:33:59 -0400

    added my meal planner section

--- client/src/pages/ProtectedRouteTwo/index.js
+++ client/src/pages/ProtectedRouteTwo/index.js
@@ -31,8 +31,60 @@ function ProtectedRoute() {
 	}, );
 	
 	return (
-		<>
-		</>
+		<div className = "weeklyPlaner">
+     <div className = "planTitle"> <h1>WEEKLY MEAL PLANNER</h1> </div>
+     <div className = "firstThree">
+         <div className="weeklyDay"><h3>Monday</h3>
+          <textarea className="textArea" type="text" name="Name" placeholder = "Breakfast:" /> 
+         <textarea className="textArea" type="text" name="Name" placeholder = "Lunch:" />
+         <textarea className="textArea" type="text" name="Name" placeholder = "Dinner:" /> 
+        </div>
+
+         <div className="weeklyDay"> <h3>Tuesday</h3>
+         <textarea className="textArea" type="text" name="Name" placeholder = "Breakfast:" /> 
+         <textarea className="textArea" type="text" name="Name" placeholder = "Lunch:" />
+         <textarea className="textArea" type="text" name="Name" placeholder = "Dinner:" /> 
+         </div>
+         <div className="weeklyDay"> <h3>Wednesday</h3>
+         <textarea className="textArea" type="text" name="Name" placeholder = "Breakfast:" /> 
+         <textarea className="textArea" type="text" name="Name" placeholder = "Lunch:" />
+         <textarea className="textArea" type="text" name="Name" placeholder = "Dinner:" /> 
+         </div>
+     </div>
+     <br/>
+     <br/>
+    
+     <div className = "nextThree">
+     <div className="weeklyDay">  <h3>Thursday</h3>
+         <textarea className="textArea" type="text" name="Name" placeholder = "Breakfast:" /> 
+         <textarea className="textArea" type="text" name="Name" placeholder = "Lunch:" />
+         <textarea className="textArea" type="text" name="Name" placeholder = "Dinner:" />  
+     </div>
+     <div className="weeklyDay">  <h3>Friday</h3>
+         <textarea className="textArea" type="text" name="Name" placeholder = "Breakfast:" /> 
+         <textarea className="textArea" type="text" name="Name" placeholder = "Lunch:" />
+         <textarea className="textArea" type="text" name="Name" placeholder = "Dinner:" /> 
+     </div>
+     <div className="weeklyDay">  <h3>Saturday</h3>
+         <textarea className="textArea" type="text" name="Name" placeholder = "Breakfast:" /> 
+         <textarea className="textArea" type="text" name="Name" placeholder = "Lunch:" />
+         <textarea className="textArea" type="text" name="Name" placeholder = "Dinner:" /> 
+     </div>
+     </div>
+     <br/>
+     <br/>
+     <div className = "lastDay">
+     <div className="weeklyDay">  <h3>Sunday</h3>
+         <textarea className="textArea" type="text" name="Name" placeholder = "Breakfast:" /> 
+         <textarea className="textArea" type="text" name="Name" placeholder = "Lunch:" />
+         <textarea className="textArea" type="text" name="Name" placeholder = "Dinner:" /> 
+     </div>
+     <div id="shoppingList">    <h3>Shopping list</h3>
+     <textarea id="textArea" type="text" name="Name" placeholder = "Shopping List:" /> 
+     </div>
+     </div>
+
+     </div>
 	)
 
 }
